7dd2b6956b5f4f7ccf602580356444c5b3fcffb3
mzb <331972146@qq.com>
2017-05-03 19:06:10 +0800

    Ugly Number

--- main.cpp
+++ main.cpp
@@ -1,52 +1,23 @@
 #include "iostream"
 #include "vector"
-#include "deque"
 
 using namespace std;
-
-struct TreeNode
-{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-
-};
 
 class Solution
 {
 public:
-    vector<vector<int>> levelOrderBottom(TreeNode* root)
+    bool isUgly(int num)
     {
-        if(root==NULL)
-            return {};
-        deque<vector<int>> ret;
-        deque<TreeNode*> tree;
-        tree.push_back(root);
-        int countCurr=1,countNext=0;
-        vector<int> tmp;
-        while(tree.size()!=0)
-        {
-            tmp.push_back(tree.front()->val);
-            countCurr--;
-            if(tree.front()->left!=NULL)
-            {
-                tree.push_back(tree.front()->left);
-                countNext++;
-            }
-            if(tree.front()->right!=NULL)
-            {
-                tree.push_back(tree.front()->right);
-                countNext++;
-            }
-            tree.pop_front();
-            if(countCurr==0)
-            {
-                ret.push_front(tmp);
-                tmp.clear();
-                countCurr=countNext;
-                countNext=0;
-            }
-        }
-        return vector<vector<int>>(ret.begin(),ret.end());
+        if(!num)
+            return false;
+        while(!(num%2))
+            num/=2;
+        while(!(num%3))
+            num/=3;
+        while(!(num%5))
+            num/=5;
+        if(num==1)
+            return true;
+        return false;
     }
 };
