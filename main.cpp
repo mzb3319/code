c01ac39b0599393c5b7873870e88b0d98c07ad50
mzb <331972146@qq.com>
2017-05-03 21:24:26 +0800

    Power of Four

--- main.cpp
+++ main.cpp
@@ -1,44 +1,14 @@
 #include "iostream"
-#include "vector"
-#include "unordered_map"
 
 using namespace std;
-
-struct TreeNode
-{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-};
 
 class Solution
 {
 public:
-    vector<int> findMode(TreeNode * root)
+    bool isPowerOfFour(int num)
     {
-        unordered_map<int,int> ret;
-        core(root,ret);
-        vector<int> r;
-        int maxCount=0;
-        for(auto itr:ret)
-        {
-            if(itr.second>maxCount)
-                maxCount=itr.second;
-        }
-        for(auto itr:ret)
-        {
-            if(itr.second==maxCount)
-                r.push_back(itr.first);
-        }
-        return r;
-    }
-private:
-    void core(TreeNode* node,unordered_map<int,int> &ret)
-    {
-        if(node==NULL)
-            return;
-        ret[node->val]++;
-        core(node->left,ret);
-        core(node->right,ret);
+        if(num<0)
+            return false;
+        return !((num-1)&num)&&(num&0x55555555);
     }
 };
