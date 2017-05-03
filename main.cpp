9aec8427bc367fafb1de76ad15aef425271033e4
mzb <331972146@qq.com>
2017-05-03 18:56:38 +0800

    Binary Tree Level Order Traversal II

--- main.cpp
+++ main.cpp
@@ -1,25 +1,52 @@
 #include "iostream"
 #include "vector"
+#include "deque"
 
 using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+};
 
 class Solution
 {
 public:
-    int maxSubArray(vector<int>& nums)
+    vector<vector<int>> levelOrderBottom(TreeNode* root)
     {
-        int ret=INT32_MIN;
-        int sum=0;
-        for(int i=0;i<nums.size();i++)
+        if(root==NULL)
+            return {};
+        deque<vector<int>> ret;
+        deque<TreeNode*> tree;
+        tree.push_back(root);
+        int countCurr=1,countNext=0;
+        vector<int> tmp;
+        while(tree.size()!=0)
         {
-            sum+=nums[i];
-            if(sum>ret)
-                ret=sum;
-            if(sum<0)
+            tmp.push_back(tree.front()->val);
+            countCurr--;
+            if(tree.front()->left!=NULL)
             {
-                sum=0;
+                tree.push_back(tree.front()->left);
+                countNext++;
+            }
+            if(tree.front()->right!=NULL)
+            {
+                tree.push_back(tree.front()->right);
+                countNext++;
+            }
+            tree.pop_front();
+            if(countCurr==0)
+            {
+                ret.push_front(tmp);
+                tmp.clear();
+                countCurr=countNext;
+                countNext=0;
             }
         }
-        return ret;
+        return vector<vector<int>>(ret.begin(),ret.end());
     }
 };
