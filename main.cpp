28842c0e39854479427da04aacaaf0d7d5ff4100
mzb <331972146@qq.com>
2017-05-03 16:53:05 +0800

    Maximum Subarray

--- main.cpp
+++ main.cpp
@@ -3,43 +3,22 @@
 
 using namespace std;
 
-struct TreeNode
-{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-};
-
 class Solution
 {
 public:
-    int pathSum(TreeNode* root,int sum)
+    int maxSubArray(vector<int>& nums)
     {
-        vector<int> path;
-        int ret=0;
-        core(root,path,ret,sum);
-        return ret;
-    }
-private:
-    void core(TreeNode* node,vector<int>& path,int &ret,int k)
-    {
-        if(node==NULL)
-            return;
-        path.push_back(node->val);
-        ret+=findSum(path,k);
-        core(node->left,path,ret,k);
-        core(node->right,path,ret,k);
-        path.pop_back();
-    }
-    int findSum(vector<int>& path,int k)
-    {
+        int ret=INT32_MIN;
         int sum=0;
-        int ret=0;
-        for(int i=path.size()-1;i>=0;i--)
+        for(int i=0;i<nums.size();i++)
         {
-            sum+=path[i];
-            if(sum==k)
-                ret++;
+            sum+=nums[i];
+            if(sum>ret)
+                ret=sum;
+            if(sum<0)
+            {
+                sum=0;
+            }
         }
         return ret;
     }
