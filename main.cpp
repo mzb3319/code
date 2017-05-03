650b5f820b3855bcf553051baca5f74807a35e89
mzb <331972146@qq.com>
2017-05-03 20:29:55 +0800

    Repeated Substring Pattern

--- main.cpp
+++ main.cpp
@@ -1,73 +1,34 @@
 #include "iostream"
-#include "vector"
+#include "string"
 
 using namespace std;
-
-struct TreeNode
-{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-
-};
 
 class Solution
 {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root,TreeNode* p,TreeNode* q)
+    bool repeatedSubstringPattern(string s)
     {
-        vector<TreeNode*>path,path1,path2;
-        core(root,p,q,path,path1,path2);
-        TreeNode* curr,*pre;
-        for(int i=0;i<path1.size();i++)
+        for(int i=1;i<=s.length()/2;i++)
         {
-            if(path1[i]!=path2[i])
-            {
-                return pre;
-            }
-            if(i==path1.size()-1||i==path2.size()-1)
-                return path1[i];
-            pre=path1[i];
+            if(s[i]==s[0])
+                if(check(s,i))
+                    return true;
         }
-        return root;
+        return false;
     }
 private:
-    void core(TreeNode* node,TreeNode* p,TreeNode* q,vector<TreeNode*>& path,vector<TreeNode*>& path1,vector<TreeNode*>& path2)
+    bool check(string &s,int index)
     {
-        if(node==NULL)
-            return;
-        path.push_back(node);
-        if(node->val==p->val||node->val==q->val)
+        if(s.length()%index)
+            return false;
+        for(int i=0;i<index;i++)
         {
-            if(find)
+            for(int j=index+i;j<s.length();j+=index)
             {
-                path2=path;
-                findAll=true;
-                return;
-            }
-            else
-            {
-                path1=path;
-                find=true;
+                if(s[j]!=s[i])
+                    return false;
             }
         }
-        if(!findAll)
-            core(node->left,p,q,path,path1,path2);
-        if(!findAll)
-            core(node->right,p,q,path,path1,path2);
-        path.pop_back();
+        return true;
     }
-    bool find=false;
-    bool findAll=false;
 };
-
-TreeNode* lowestCommonAncestor(TreeNode*root,TreeNode* p,TreeNode* q)
-{
-    if(root==NULL)
-        return NULL;
-    if(p->val>root->val&&q->val>root->val)
-        return lowestCommonAncestor(root->right,p,q);
-    if(p->val<root->val&&q->val<root->val)
-        return lowestCommonAncestor(root->left,p,q);
-    return root;
-}
