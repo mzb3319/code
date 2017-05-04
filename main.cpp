8e130c8ecf78abe2463dd9effb0901129e760be2
mzb <331972146@qq.com>
2017-05-04 17:03:45 +0800

    Binary Tree Paths

--- main.cpp
+++ main.cpp
@@ -1,19 +1,40 @@
 #include "iostream"
 #include "string"
+#include "vector"
 
 using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+};
 
 class Solution
 {
 public:
-    int countSegments(string s)
+    vector<string> binaryTreePaths(TreeNode * root)
     {
-        int ret=0;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s[i]==' '&&i+1<s.length()&&s[i+1]!=' ')
-                ret++;
-        }
+        if(root==NULL)
+            return {};
+        string path;
+        vector<string> ret;
+        core(root,path,ret);
         return ret;
+    }
+private:
+    void core(TreeNode* node,string & path,vector<string>& ret)
+    {
+        if(node!=NULL)
+            path+="->"+to_string(node->val);
+        if(node->left==NULL&&node->right==NULL)
+            ret.push_back(string(path.begin()+2,path.end()));
+        if(node->left!=NULL)
+            core(node->left,path,ret);
+        if(node->right!=NULL)
+            core(node->right,path,ret);
+        path.erase(path.find_last_of("->"));
     }
 };
