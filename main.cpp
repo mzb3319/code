954d9f46d9211dd40780e2da2f61f40105b8c847
mzb <331972146@qq.com>
2017-05-04 16:09:52 +0800

    Number of Segments in a String

--- main.cpp
+++ main.cpp
@@ -1,28 +1,18 @@
 #include "iostream"
-#include "vector"
+#include "string"
 
 using namespace std;
 
 class Solution
 {
 public:
-    vector<vector<int>> generate(int numRows)
+    int countSegments(string s)
     {
-        if(numRows==0)
-            return {};
-        vector<vector<int>> ret{{1}};
-        for(int i=1;i<numRows;i++)
+        int ret=0;
+        for(int i=0;i<s.length();i++)
         {
-            vector<int> tmp;
-            for(int j=0;j<ret[i-1].size();j++)
-            {
-                if(j-1>=0)
-                    tmp.push_back(ret[i-1][j-1]+ret[i-1][j]);
-                else
-                    tmp.push_back(ret[i-1][j]);
-            }
-            tmp.push_back(1);
-            ret.push_back(tmp);
+            if(s[i]==' '&&i+1<s.length()&&s[i+1]!=' ')
+                ret++;
         }
         return ret;
     }
