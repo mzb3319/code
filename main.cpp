d8f5115679286a59c7b5d42a9abc64d3063baacb
mzb <331972146@qq.com>
2017-05-04 14:41:49 +0800

    Reverse Vowels of a String

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include "iostream"
+#include "string"
+#include "unordered_set"
 
 using namespace std;
 
 class Solution
 {
 public:
-    bool isPowerOfFour(int num)
+    string reverseVowels(string s)
     {
-        if(num<0)
-            return false;
-        return !((num-1)&num)&&(num&0x55555555);
+        unordered_set<char> table{'a','e','i','o','u','A','E','I','O','U'};
+        int beg=0,end=s.length()-1;
+        while(beg<=end)
+        {
+            if(table.find(s[beg])!=table.end()&&table.find(s[end])!=table.end())
+            {
+                char tmp=s[beg];
+                s[beg]=s[end];
+                s[end]=tmp;
+                beg++;
+                end--;
+            }
+            else if(table.find(s[beg])==table.end())
+                beg++;
+            else
+                end--;
+        }
+        return s;
     }
 };
