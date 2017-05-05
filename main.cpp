33662284ec7349d732b12cc663b5abd655b6af66
mzb <331972146@qq.com>
2017-05-05 17:54:53 +0800

    Minimum Depth of Binary Tree

--- main.cpp
+++ main.cpp
@@ -1,40 +1,31 @@
 #include "iostream"
-#include "unordered_set"
-#include "cmath"
 
 using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+};
 
 
 class Solution
 {
 public:
-    bool checkPerfectNumber(int num)
-    {
-        int sum=1;
-        int i=2;
-        double min=sqrt(num);
-        if(min/1.0>0.00000001)
-            min++;
-        int m=min;
-        while(i<m)
-        {
-            if(num%i==0)
-            {
-                sum+=i;
-                int tmp=num/i;
-                sum+=tmp;
-            }
-            if(sum>num)
-                return false;
-            i++;
-        }
-        return sum==num;
-    }
+     int minDepth(TreeNode* root)
+     {
+         if(root==NULL)
+             return 0;
+         if(root->left==NULL&&root->right==NULL)
+             return 1;
+         int leftD=INT32_MAX;
+         int rightD=INT32_MAX;
+         if(root->left!=NULL)
+             leftD=minDepth(root->left);
+         if(root->right!=NULL)
+             rightD=minDepth(root->right);
+         return leftD>rightD?rightD+1:leftD+1;
+     }
 };
-
-int main()
-{
-    Solution s;
-    s.checkPerfectNumber(6);
-    return 0;
-}
