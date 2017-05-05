e92027c346ad2cce26e816e7578637b7928636bf
mzb <331972146@qq.com>
2017-05-05 14:24:39 +0800

    Find All Anagrams in a String

--- main.cpp
+++ main.cpp
@@ -1,33 +1,59 @@
 #include "iostream"
+#include "string"
 #include "vector"
+#include "unordered_map"
 
 using namespace std;
 
-struct TreeNode
-{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-};
+
 
 class Solution
 {
 public:
-    bool hasPathSum(TreeNode* root,int sum)
+    vector<int> findAnagrams(string s,string p)
     {
-        if(root==NULL)
+        if(s.length()<p.length())
+            return {};
+        unordered_map<char,int> table,table1;
+        vector<int> ret;
+        for(int i=0;i<p.length();i++)
         {
-            return false;
+            table[p[i]]++;
+            table1[p[i]]++;
         }
-        if(root->left==NULL&&root->right==NULL&&sum==root->val)
-            return true;
-        else
+        int pL=p.length();
+        for(int p=0,l=0;p<s.length();p++)
         {
-            if(hasPathSum(root->left,sum-root->val))
-                return true;
-            if(hasPathSum(root->right,sum-root->val))
-                return true;
+            while(p==l&&table.find(s[p])==table.end())
+            {
+                p++;
+                l++;
+                if(p>=s.length())
+                    break;
+            }
+            auto f=table.find(s[p]);
+            if(f==table.end())
+            {
+                l=p+1;
+                table=table1;
+                continue;
+            }
+            if(f->second==0)
+            {
+                p=l;
+                l++;
+                table=table1;
+                continue;
+            }
+            int diff=p-l+1;
+            f->second--;
+            if(diff==pL)
+            {
+                ret.push_back(l);
+                table[s[l]]++;
+                l++;
+            }
         }
-        return false;
+        return ret;
     }
 };
