6702da253b96f7548d2b0849080791351ea500ac
mzb <331972146@qq.com>
2017-05-09 20:35:44 +0800

    Remove Linked List Elements

--- main.cpp
+++ main.cpp
@@ -6,68 +6,31 @@ struct ListNode
 {
     int val;
     ListNode* next;
+    ListNode(int x):val(x),next(NULL)
+    {}
 };
 
 class Solution
 {
 public:
-    bool isPalindrome(ListNode* head)
+    ListNode* removeElements(ListNode* head,int val)
     {
-        if(head==NULL)
-            return true;
-        int len=0;
-        bool step=false;
-        ListNode* h=head;
-        while(h!=NULL)
+        ListNode p(0);
+        p.next=head;
+        ListNode *curr=head,*pre=&p;
+        while(curr!=NULL)
         {
-            len++;
-            h=h->next;
-        }
-        if(len%2)
-            step=true;
-        len/=2;
-        if(len>=2)
-        {
-            ListNode*h_old=head,*pre=NULL,*curr=head,*nex=head->next;
-            int count=1;
-            while(count<len)
+            if(curr->val==val)
             {
-                curr->next=pre;
-                pre=curr;
-                curr=nex;
-                nex=nex->next;
-                count++;
+                pre->next=curr->next;
+                delete curr;
             }
-            curr->next=pre;
-            head=curr;
-            h_old->next=nex;
+            else
+            {
+                pre=curr;
+            }
+            curr=pre->next;
         }
-        h=head;
-        while(len)
-        {
-            h=h->next;
-            len--;
-        }
-        if(step)
-            h=h->next;
-        while(h!=NULL)
-        {
-            if(head->val!=h->val)
-                return false;
-            h=h->next;
-            head=head->next;
-        }
-        return true;
+        return p.next;
     }
 };
-
-int main()
-{
-    ListNode *head,a,b,c,d,e;
-    a.val=1;b.val=2;c.val=3;d.val=2;e.val=1;
-    a.next=&b;b.next=&c;c.next=&d;d.next=&e;e.next=NULL;
-    head=&a;
-    Solution s;
-    s.isPalindrome(head);
-    return 0;
-}
