c199a935f3fcaa7e824752630a527ff3671997d9
mzb <331972146@qq.com>
2017-05-09 10:25:25 +0800

    Word Pattern

--- main.cpp
+++ main.cpp
@@ -1,31 +1,39 @@
 #include "iostream"
+#include "string"
+#include "unordered_map"
+#include "sstream"
 
 using namespace std;
-
-struct TreeNode
-{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-
-};
-
 
 class Solution
 {
 public:
-     int minDepth(TreeNode* root)
-     {
-         if(root==NULL)
-             return 0;
-         if(root->left==NULL&&root->right==NULL)
-             return 1;
-         int leftD=INT32_MAX;
-         int rightD=INT32_MAX;
-         if(root->left!=NULL)
-             leftD=minDepth(root->left);
-         if(root->right!=NULL)
-             rightD=minDepth(root->right);
-         return leftD>rightD?rightD+1:leftD+1;
-     }
+    bool wordPattern(string pattern,string str)
+    {
+        unordered_map<char,string> pattern2str;
+        unordered_map<string,char> str2pattern;
+        istringstream s(str);
+        string word;
+        int i=0;
+        while(s>>word)
+        {
+            if(i>=pattern.length())
+                return false;
+            auto fp=pattern2str.find(pattern[i]);
+            auto fs=str2pattern.find(word);
+            if(fp==pattern2str.end()&&fs==str2pattern.end())
+            {
+                pattern2str[pattern[i]]=word;
+                str2pattern[word]=pattern[i];
+            }
+            else if(fp!=pattern2str.end()&&fp->second==word&&fs!=str2pattern.end()&&fs->second==pattern[i])
+            {
+                ;
+            }
+            else
+                return false;
+            i++;
+        }
+        return i==pattern.length();
+    }
 };
