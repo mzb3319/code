edcefe3640ede6ef830a272c3ac18439ff048fd4
mzb <331972146@qq.com>
2017-05-09 16:48:21 +0800

    Palindrome Linked List

--- main.cpp
+++ main.cpp
@@ -1,39 +1,73 @@
 #include "iostream"
-#include "string"
-#include "unordered_map"
-#include "sstream"
 
 using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode* next;
+};
 
 class Solution
 {
 public:
-    bool wordPattern(string pattern,string str)
+    bool isPalindrome(ListNode* head)
     {
-        unordered_map<char,string> pattern2str;
-        unordered_map<string,char> str2pattern;
-        istringstream s(str);
-        string word;
-        int i=0;
-        while(s>>word)
+        if(head==NULL)
+            return true;
+        int len=0;
+        bool step=false;
+        ListNode* h=head;
+        while(h!=NULL)
         {
-            if(i>=pattern.length())
-                return false;
-            auto fp=pattern2str.find(pattern[i]);
-            auto fs=str2pattern.find(word);
-            if(fp==pattern2str.end()&&fs==str2pattern.end())
-            {
-                pattern2str[pattern[i]]=word;
-                str2pattern[word]=pattern[i];
-            }
-            else if(fp!=pattern2str.end()&&fp->second==word&&fs!=str2pattern.end()&&fs->second==pattern[i])
-            {
-                ;
-            }
-            else
-                return false;
-            i++;
+            len++;
+            h=h->next;
         }
-        return i==pattern.length();
+        if(len%2)
+            step=true;
+        len/=2;
+        if(len>=2)
+        {
+            ListNode*h_old=head,*pre=NULL,*curr=head,*nex=head->next;
+            int count=1;
+            while(count<len)
+            {
+                curr->next=pre;
+                pre=curr;
+                curr=nex;
+                nex=nex->next;
+                count++;
+            }
+            curr->next=pre;
+            head=curr;
+            h_old->next=nex;
+        }
+        h=head;
+        while(len)
+        {
+            h=h->next;
+            len--;
+        }
+        if(step)
+            h=h->next;
+        while(h!=NULL)
+        {
+            if(head->val!=h->val)
+                return false;
+            h=h->next;
+            head=head->next;
+        }
+        return true;
     }
 };
+
+int main()
+{
+    ListNode *head,a,b,c,d,e;
+    a.val=1;b.val=2;c.val=3;d.val=2;e.val=1;
+    a.next=&b;b.next=&c;c.next=&d;d.next=&e;e.next=NULL;
+    head=&a;
+    Solution s;
+    s.isPalindrome(head);
+    return 0;
+}
