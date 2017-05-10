64b4ec3492b739d9246ac4305cbfca8cc0e18c93
mzb <331972146@qq.com>
2017-05-10 15:58:21 +0800

    Reverse Bits

--- main.cpp
+++ main.cpp
@@ -1,44 +1,21 @@
 #include "iostream"
-#include "vector"
-#include "algorithm"
 
 using namespace std;
 
 class Solution
 {
 public:
-    int findRadius(vector<int> &houses,vector<int>& heaters)
+    uint32_t reverseBits(uint32_t n)
     {
-        if(houses.size()==0||heaters.size()==0)
-            return -1;
-        sort(houses.begin(),houses.end());
-        sort(heaters.begin(),heaters.end());
-        int radius=0;
-        int a=0,b=0;
-        int nextHeater=b;
-        for(;a<houses.size();a++)
+        uint32_t flag=0;
+        uint32_t ret=0;
+        for(int i=0;i<32;i++)
         {
-            while(houses[a]>heaters[nextHeater])
-            {
-                b=nextHeater;
-                if(nextHeater<heaters.size()-1)
-                    nextHeater++;
-                else
-                    break;
-            }
-            int diff=0;
-            if(houses[a]<heaters[b]&&houses[a]<heaters[nextHeater])
-                diff=heaters[b]-houses[a];
-            else if(houses[a]>heaters[b]&&houses[a]>heaters[nextHeater])
-                diff=houses[a]-heaters[b];
-            else if(houses[a]>heaters[b]&&houses[a]<heaters[nextHeater])
-            {
-                int diff1=houses[a]-heaters[b],diff2=heaters[nextHeater]-houses[a];
-                diff=diff1<diff2?diff1:diff2;
-            }
-            if(radius<diff)
-                radius=diff;
+            flag=n&1;
+            ret=ret<<1;
+            ret=ret|flag;
+            n=n>>1;
         }
-        return radius;
+        return ret;
     }
 };
