e1804c9f75a228120511d3af4f5ac9eebb56cb0a
mzb <mzb3319@163.com>
2017-05-15 12:13:30 +0800

    Complex Number Multiplication

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
 #include "iostream"
-#include "vector"
 #include "string"
 
 using namespace std;
 
+
 class Solution
 {
 public:
-    string convertToTitle(int n)
+    string complexNumberMultiply(string a,string b)
     {
-        string ret;
-        while(n)
-        {
-            char tmp='A'+(n-1)%26;
-            ret=tmp+ret;
-            n=(n-1)/26;
-        }
-        return ret;
+        int a1=0,b1=0,a2=0,b2=0;
+        auto f=a.find('+');
+        string tmp;
+        tmp=a.substr(0,f);
+        a1=stoi(tmp);
+        tmp=a.substr(f+1,a.size()-f-2);
+        a2=stoi(tmp);
+        f=b.find('+');
+        tmp=b.substr(0,f);
+        b1=stoi(tmp);
+        tmp=b.substr(f+1,b.size()-f-2);
+        b2=stoi(tmp);
+        int r=a1*b1-a2*b2;
+        int i=a1*b2+a2*b1;
+        return to_string(r)+'+'+to_string(i)+'i';
     }
 };
