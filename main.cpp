4af411e8dd51d79d7fcfc540a109c564a8e7dd3e
mzb <mzb3319@163.com>
2017-05-16 15:26:50 +0800

    Distribute Candies

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "unordered_set"
 #include "iostream"
 #include "vector"
 
@@ -6,35 +7,14 @@ using namespace std;
 class Solution
 {
 public:
-    vector<vector<int>> matrixReshape(vector<vector<int>> & nums,int r,int c)
+    int distributeCandies(vector<int>& candies)
     {
-        if(nums.empty()||nums[0].empty()||nums.size()*nums[0].size()!=r*c)
-            return nums;
-        vector<vector<int>> ret;
-        vector<int> tmp;
-        int count=0;
-        for(int i=0;i<nums.size();i++)
+        int num=candies.size()/2;
+        unordered_set<int> gain;
+        for(int candy:candies)
         {
-            for(int j=0;j<nums[0].size();j++)
-            {
-                tmp.push_back(nums[i][j]);
-                count++;
-                if(count==c)
-                {
-                    ret.push_back(tmp);
-                    tmp.clear();
-                    count=0;
-                }
-            }
+            gain.insert(candy);
         }
-        return ret;
+        return gain.size()>num?num:gain.size();
     }
 };
-
-int main()
-{
-    vector<vector<int>> table{{1,2},{3,4}};
-    Solution s;
-    s.matrixReshape(table,4,1);
-    return 0;
-}
