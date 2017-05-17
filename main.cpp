f810ffba6a48d2b98d15dcb25048fbf769cf23c0
mzb <mzb3319@163.com>
2017-05-17 21:39:09 +0800

    Convert BST to Greater Tree

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
-#include <vector>
 #include "iostream"
-#include "deque"
 
 using namespace std;
 
@@ -12,45 +10,22 @@ struct TreeNode
 
 };
 
+
 class Solution
 {
 public:
-    vector<int> largestValues(TreeNode* root)
+    TreeNode* convertBST(TreeNode* root)
     {
-        if(root==NULL)
-            return {};
-        int currC=1,nextC=0;
-        vector<int> ret;
-        deque<TreeNode*> tree{root};
-        int max=root->val;
-        while(!tree.empty())
-        {
-            if(currC>0)
-            {
-                if(tree.front()->val>max)
-                    max=tree.front()->val;
-                if(tree.front()->left!=NULL)
-                {
-                    nextC++;
-                    tree.push_back(tree.front()->left);
-                }
-                if(tree.front()->right!=NULL)
-                {
-                    nextC++;
-                    tree.push_back(tree.front()->right);
-                }
-                currC--;
-                if(currC==0)
-                    ret.push_back(max);
-                tree.pop_front();
-            }
-            else
-            {
-                max=INT32_MIN;
-                currC=nextC;
-                nextC=0;
-            }
-        }
-        return ret;
+        core(root,0);
+        return root;
+    }
+private:
+    int core(TreeNode* node,int carry)
+    {
+        if(node==NULL)
+            return carry;
+        node->val+=core(node->right,carry);
+        int left=core(node->left,node->val);
+        return left;
     }
 };
