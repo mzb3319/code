78dacc78df551b957f5746ca7c13bebcc7fbe0bd
mzb <mzb3319@163.com>
2017-05-18 18:14:29 +0800

    Minesweeper

--- main.cpp
+++ main.cpp
@@ -1,46 +1,59 @@
 #include "iostream"
 #include "vector"
-#include "unordered_map"
 
 using namespace std;
-
-struct TreeNode
-{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-};
 
 class Solution
 {
 public:
-    vector<int> findFrequentTreeSum(TreeNode* root)
+    vector<vector<char>> updateBoard(vector<vector<char>>& board,vector<int>& click)
     {
-        unordered_map<int,int> table;
-        core(root,table);
-        int max=INT32_MIN;
-        for(auto t:table)
+        if(board[click[0]][click[1]]=='M')
         {
-            if(t.second>max)
-                max=t.second;
+            board[click[0]][click[1]]='X';
+            return board;
         }
-        vector<int> ret;
-        for(auto t:table)
-        {
-            if(t.second==max)
-                ret.push_back(t.first);
-        }
-        return ret;
+        core(board,click);
+        return board;
     }
 private:
-    int core(TreeNode* node,unordered_map<int,int>& table)
+    void core(vector<vector<char>>& board,vector<int> pos)
     {
-        if(node==NULL)
-            return 0;
-        int left=core(node->left,table);
-        int right=core(node->right,table);
-        int sum=node->val+left+right;
-        table[sum]++;
-        return sum;
+        int x=pos[0],y=pos[1];
+        if(x<0||y<0||x>=board.size()||y>=board[0].size())
+            return;
+        if(board[x][y]=='E')
+        {
+            board[x][y] = 'B';
+            int count = 0;
+            if (x - 1 >= 0 && board[x - 1][y] == 'M')//top
+                count++;
+            if (x + 1 < board.size() && board[x + 1][y] == 'M')//bottom
+                count++;
+            if (y - 1 >= 0 && board[x][y - 1] == 'M')//left
+                count++;
+            if (y + 1 < board[0].size() && board[x][y + 1] == 'M')//right
+                count++;
+            if (x - 1 >= 0 && y - 1 >= 0 && board[x - 1][y - 1] == 'M')//left-top
+                count++;
+            if (x - 1 >= 0 && y + 1 < board[0].size() && board[x - 1][y + 1] == 'M')//right-top
+                count++;
+            if (x + 1 < board.size() && y - 1 >= 0 && board[x + 1][y - 1] == 'M')//left-bottom
+                count++;
+            if (x + 1 < board.size() && y + 1 < board[0].size() && board[x + 1][y + 1] == 'M')//right-bottom
+                count++;
+            if (count)
+                board[x][y] = '0' + count;
+            else {
+                core(board, {x - 1, y});
+                core(board, {x + 1, y});
+                core(board, {x, y - 1});
+                core(board, {x, y + 1});
+                core(board, {x - 1, y - 1});
+                core(board, {x - 1, y + 1});
+                core(board, {x + 1, y - 1});
+                core(board, {x + 1, y + 1});
+            }
+        }
     }
 };
