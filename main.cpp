028a3b1853ece9f762fc8b7dbf55f1c420302c6c
mzb <mzb3319@163.com>
2017-05-19 19:57:52 +0800

    Top K Frequent Elements

--- main.cpp
+++ main.cpp
@@ -1,26 +1,39 @@
 #include "iostream"
 #include "vector"
+#include "algorithm"
+#include "unordered_map"
 
 using namespace std;
 
 class Solution
 {
 public:
-    vector<int> productExceptSelf(vector<int>& nums)
+    vector<int> topKFrequent(vector<int>& nums,int k)
     {
-        vector<int> ret(nums.size(),1);
-        int muti=1;
-        for(int i=0;i<nums.size();i++)
+        unordered_map<int,int> table;
+        for(int num:nums)
         {
-            ret[i]=muti;
-            muti*=nums[i];
+            table[num]++;
         }
-        muti=1;
-        for(int i=nums.size()-1;i>=0;i--)
+        vector<pair<int,int>> map(table.begin(),table.end());
+        sort(map.begin(),map.end(),[](pair<int,int> &a,pair<int,int>& b){return a.second>b.second;});
+        vector<int> ret;
+        int i=0;
+        for(auto itr:map)
         {
-            ret[i]*=muti;
-            muti*=nums[i];
+            if(i==k)
+                break;
+            i++;
+            ret.push_back(itr.first);
         }
         return ret;
     }
 };
+
+int main()
+{
+    vector<int> nums{1,2,2,3,3,3};
+    Solution s;
+    s.topKFrequent(nums,2);
+    return 0;
+}
