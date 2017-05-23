41ccf78c1faa9872150850ecc440eabc6990c047
mzb <mzb3319@163.com>
2017-05-23 18:55:53 +0800

    Next Greater Element II

--- main.cpp
+++ main.cpp
@@ -1,39 +1,26 @@
 #include "iostream"
 #include "vector"
-#include "algorithm"
-#include "unordered_map"
 
 using namespace std;
 
 class Solution
 {
 public:
-    vector<int> topKFrequent(vector<int>& nums,int k)
+    vector<int> nextGreaterElements(vector<int> & nums)
     {
-        unordered_map<int,int> table;
-        for(int num:nums)
+        vector<int> table,ret(nums.size(),-1);
+        int len=nums.size();
+        for(int i=0;i<2*len;i++)
         {
-            table[num]++;
-        }
-        vector<pair<int,int>> map(table.begin(),table.end());
-        sort(map.begin(),map.end(),[](pair<int,int> &a,pair<int,int>& b){return a.second>b.second;});
-        vector<int> ret;
-        int i=0;
-        for(auto itr:map)
-        {
-            if(i==k)
-                break;
-            i++;
-            ret.push_back(itr.first);
+            int num=nums[i%len];
+            while(!table.empty()&&nums[table.back()]<num)
+            {
+                ret[table.back()]=num;
+                table.pop_back();
+            }
+            if(i<len)
+                table.push_back(i);
         }
         return ret;
     }
 };
-
-int main()
-{
-    vector<int> nums{1,2,2,3,3,3};
-    Solution s;
-    s.topKFrequent(nums,2);
-    return 0;
-}
