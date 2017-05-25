e0caab0d4d06b82b166716a51cb6dcff544602af
mzb <mzb3319@163.com>
2017-05-25 21:45:45 +0800

    Magical String

--- main.cpp
+++ main.cpp
@@ -1,40 +1,36 @@
 #include "iostream"
-#include "string"
 #include "vector"
-#include "algorithm"
 
 using namespace std;
 
 class Solution
 {
 public:
-    int findMinDifference(vector<string>& timePoints)
+    int magicalString(int n)
     {
-        sort(timePoints.begin(),timePoints.end());
-        int diff=INT32_MAX;
-        for(int i=1;i<timePoints.size();i++)
+        if(!n)
+            return 0;
+        int ret=1;
+        vector<int> ms{1};
+        vector<int> table{0,2,1};
+        int index=0,preChar=1,dum=1;
+        while(ms.size()<n)
         {
-            if(timePoints[i]==timePoints[i-1])
-                return 0;
-            int h1=stoi(timePoints[i]);
-            int h2=stoi(timePoints[i-1]);
-            int m1=stoi(timePoints[i].substr(3,2));
-            int m2=stoi(timePoints[i-1].substr(3,2));
-            int d=(h1-h2)*60+m1-m2;
-            if(d<diff)
-                diff=d;
+            if(ms[index]==dum)//change a num to add
+            {
+                ms.push_back(table[preChar]);
+                dum=1;
+                index++;
+            }
+            else
+            {
+                ms.push_back(preChar);
+                dum++;
+            }
+            preChar=ms.back();
+            if(preChar==1)
+                ret++;
         }
-        int d=(23-stoi(timePoints.back()))*60+60-stoi(timePoints.back().substr(3,2))+stoi(timePoints.front())*60+stoi(timePoints.front().substr(3,2));
-        if(d<diff)
-            diff=d;
-        return diff;
+        return ret;
     }
 };
-
-int main()
-{
-    Solution s;
-    vector<string> time{"12:12","00:13"};
-    s.findMinDifference(time);
-    return 0;
-}
