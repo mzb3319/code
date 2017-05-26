c568ac55da620be564c7142aa1492a76eab6b1c4
mzb <mzb3319@163.com>
2017-05-26 20:26:53 +0800

    Is Subsequence

--- main.cpp
+++ main.cpp
@@ -1,34 +1,29 @@
 #include "iostream"
 #include "vector"
+#include "string"
 
 using namespace std;
 
 class Solution
 {
 public:
-    bool PredictTheWinner(vector<int>& nums)
+    bool isSubsequence(string &s,string &t)
     {
-        int table[20][20];
-        int all=0;
-        for(int i=0;i<nums.size();i++)
+        int i=0,j=0;
+        for(;i<s.length();)
         {
-            table[i][i]=nums[i];
-            all+=nums[i];
-        }
-        for(int i=2;i<=nums.size();i++)
-        {
-            for(int j=0;j<=nums.size()-i;j++)
+            if(j==t.length())
+                break;
+            for(;j<t.length();j++)
             {
-                int end=j+i-1;
-                int add=0;
-                for(int k=j;k<=end;k++)
-                    add+=nums[k];
-                int m=min(table[j+1][end],table[j][end-1]);
-                table[j][end]=add-m;
+                if(s[i]==t[j])
+                {
+                    j++;
+                    i++;
+                    break;
+                }
             }
         }
-        if(table[0][nums.size()-1]>=(all/2+all%2))
-            return true;
-        return false;
+        return i==s.length();
     }
 };
