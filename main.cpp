3408fa6857d6352114bb195b9ddea8a3c5f0310f
mzb <mzb3319@163.com>
2017-06-01 18:13:17 +0800

    Reconstruct Original Digits from English

--- main.cpp
+++ main.cpp
@@ -1,36 +1,104 @@
-#include "bitset"
 #include "iostream"
-#include "vector"
 #include "string"
+#include "unordered_map"
 
 using namespace std;
 
 class Solution
 {
 public:
-    int maxProduct(vector<string> &words)
+    string originalDigits(string &s)
     {
-        vector<bitset<26>> table(words.size(),0);
-        for(int i=0;i<words.size();i++)
+        unordered_map<char,int> table;
+        for(char c:s)
         {
-            for(int j=0;j<words[i].length();j++)
-            {
-                table[i].set(words[i][j]-'a');
-            }
+            table[c]++;
         }
-
-        int ret=0;
-        for(int i=0;i<table.size();i++)
+        unordered_map<int,int> nums;
+        nums[8]=core(table,'g');
+        if(nums[8])
         {
-            for(int j=i+1;j<table.size();j++)
-            {
-                if((table[i]&table[j]).none())
-                {
-                    int len=words[i].length()*words[j].length();
-                    if(ret<len)
-                        ret=len;
-                }
-            }
+            table['e']-=nums[8];
+            table['i']-=nums[8];
+            table['h']-=nums[8];
+            table['t']-=nums[8];
+        }
+        nums[4]=core(table,'u');
+        if(nums[4])
+        {
+            table['f']-=nums[4];
+            table['o']-=nums[4];
+            table['r']-=nums[4];
+        }
+        nums[6]=core(table,'x');
+        if(nums[6])
+        {
+            table['s']-=nums[6];
+            table['i']-=nums[6];
+        }
+        nums[7]=core(table,'s');
+        if(nums[7])
+        {
+            table['e']-=2*nums[7];
+            table['v']-=nums[7];
+            table['n']-=nums[7];
+        }
+        nums[5]=core(table,'f');
+        if(nums[5])
+        {
+            table['i']-=nums[5];
+            table['v']-=nums[5];
+            table['e']-=nums[5];
+        }
+        nums[9]=core(table,'i');
+        if(nums[9])
+        {
+            table['n']-=2*nums[9];
+            table['e']-=nums[9];
+        }
+        nums[3]=core(table,'h');
+        if(nums[3])
+        {
+            table['t']-=nums[3];
+            table['r']-=nums[3];
+            table['e']-=2*nums[3];
+        }
+        nums[2]=core(table,'w');
+        if(nums[2])
+        {
+            table['t']-=nums[2];
+            table['o']-=nums[2];
+        }
+        nums[0]=core(table,'z');
+        if(nums[0])
+        {
+            table['e']-=nums[0];
+            table['r']-=nums[0];
+            table['o']-=nums[0];
+        }
+        nums[1]=core(table,'o');
+        if(nums[1])
+        {
+            table['n']-=nums[1];
+            table['e']-=nums[1];
+        }
+        string ret;
+        for(int i=0;i<10;i++)
+        {
+            ret+=string(nums[i],i+'0');
+        }
+        return ret;
+    }
+
+private:
+    int core(unordered_map<char,int> &table,char c)
+    {
+        auto f=table.find(c);
+        int ret=0;
+        if(f!=table.end())
+        {
+            ret=f->second;
+            table.erase(c);
         }
         return ret;
     }
