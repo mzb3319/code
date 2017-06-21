e755aef64b2d75932f1f4fd17716fedc8c9f33e0
mzb <331972146@qq.com>
2017-06-21 22:15:11 +0800

    Random Pick Index

--- main.cpp
+++ main.cpp
@@ -1,78 +1,37 @@
-#include "iostream"
 #include "vector"
-#include "string"
-#include "deque"
+#include "iostream"
 
 using namespace std;
 
-struct TreeNode{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int a):val(a),left(NULL),right(NULL)
-    {};
-};
-
-class Codec
+class Solution
 {
 public:
-    string serialize(TreeNode *root)
+    Solution(vector<int> nums)
     {
-        string ret;
-        f(ret,root);
-        return ret;
+        NUM=nums;
     }
 
-    TreeNode *deserialize(string data)
+    int pick(int target)
     {
-        int index=0;
-        TreeNode * ret=d(data,index);
+        int ret=-1;
+        int count=0;
+        for(int i=0;i<NUM.size();i++)
+        {
+            if(NUM[i]!=target)
+                continue;
+            if(!count)
+            {
+                count++;
+                ret=i;
+            }
+            else
+            {
+                if(rand()%count==0)
+                    ret=i;
+            }
+        }
         return ret;
     }
 private:
-    void f(string &str,TreeNode *node)
-    {
-        if(!node)
-        {
-            str+="null|";
-            return;
-        }
-        str+=to_string(node->val)+'|';
-
-        f(str,node->left);
-        f(str,node->right);
-    }
-    TreeNode *d(string &str,int &index)
-    {
-        string next=nextNum(str,index);
-        if(next=="null")
-        {
-            return NULL;
-        }
-        TreeNode *node=new TreeNode(stoi(next));
-        if(index<str.size())
-            node->left=d(str,index);
-        if(index<str.size())
-            node->right=d(str,index);
-        return node;
-    }
-    string nextNum(string &str,int &index)
-    {
-        string ret;
-        int i=str.find_first_of('|',index);
-        if(i==str.size()-1)
-            return "null";
-        ret=str.substr(index,i-index);
-        index=i+1;
-        return ret;
-    }
+    vector<int> NUM;
 };
-
-int main()
-{
-    string str("2|1|null|null|3|null|null|");
-    Codec code;
-    auto r=code.deserialize(str);
-    code.serialize(r);
-    return 0;
-}
