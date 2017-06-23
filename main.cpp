949902329f9d6228345bede9a6303f3912bf86a8
mzb <331972146@qq.com>
2017-06-23 20:45:37 +0800

    Longest Word in Dictionary through Deleting

--- main.cpp
+++ main.cpp
@@ -1,37 +1,37 @@
 #include "iostream"
+#include "string"
 #include "vector"
-#include "unordered_map"
-#include "unordered_set"
+
 
 using namespace std;
 
 class Solution
 {
 public:
-    int combinationSum4(vector<int> &nums,int target)
+    string findLongestWord(string s,vector<string> &d)
     {
-        unordered_map<int,int> table;
-        return core(nums,target,table);
-    }
-private:
-    int core(vector<int> &nums,int target,unordered_map<int,int> &table)
-    {
-        auto f=table.find(target);
-        if(f!=table.end())
-            return f->second;
-        int count=0;
-        for(int i=0;i<nums.size();i++)
+        vector<int> index(d.size());
+        string ret;
+        for(int i=0;i<s.length();i++)
         {
-            if(nums[i]==target)
+            for(int j=0;j<d.size();j++)
             {
-                count++;
-                continue;
+                if(index[j]==-1)
+                    continue;
+                if(d[j][index[j]]==s[i])
+                {
+                    index[j]++;
+                    if(index[j]==d[j].size())
+                    {
+                        if(d[j].length()>ret.length()||(d[j].length()==ret.length()&&d[j]<ret))
+                        {
+                            ret=d[j];
+                        }
+                        index[j]=-1;
+                    }
+                }
             }
-            else if(nums[i]>target)
-                continue;
-            count+=core(nums,target-nums[i],table);
         }
-        table.insert({target,count});
-        return count;
+        return ret;
     }
 };
