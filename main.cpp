66f6c6e2531ffa1f55ba6292fcc0ca45201234dc
mzb <331972146@qq.com>
2017-07-03 19:57:27 +0800

    Frog Jump

--- main.cpp
+++ main.cpp
@@ -1,66 +1,91 @@
 #include "iostream"
-#include "string"
+#include "unordered_map"
+#include "vector"
 
 using namespace std;
 
 class Solution
 {
 public:
-    string decodeString(string s)
+    bool canCross(vector<int> &stones)
     {
-        return core(s,0,s.length()-1);
+        unordered_map<string,bool> table;
+        if(stones.size()<=1)
+            return true;
+        return core(stones,1,1,table);
     }
 
 private:
-    string core(string &s,int b,int e)
+    bool core(vector<int> &stones,int index,int step,unordered_map<string,bool> &table)
     {
-        string num;
-        string text;
-        string ret;
-        for(int i=b;i<=e;i++)
+        int Step=step+stones[index-1];
+        int old_index=index-1;
+        while(index<stones.size()&&stones[index]<Step)
         {
-            if(isdigit(s[i]))
-            {
-                num+=s[i];
-            }
-            else if(isalpha(s[i]))
-            {
-                text+=s[i];
-            }
-            else if(s[i]=='[')
-            {
-                int beg=i+1;
-                int count=1;
-                while(i<=e)
-                {
-                    i++;
-                    if(s[i]=='[')
-                        count++;
-                    else if(s[i]==']')
-                        count--;
-                    if(!count)
-                        break;
-                }
-                string r=core(s,beg,i-1);
-                int n=stoi(num);
-                ret+=text;
-                text.clear();
-                while(n--)
-                {
-                    ret+=r;
-                }
-                num.clear();
-            }
+            index++;
         }
-        ret+=text;
-        return ret;
+        if(index==stones.size())
+            return false;
+
+        if(stones[index]!=step+stones[old_index])
+        {
+            table.insert({to_string(stones[old_index])+'-'+to_string(step),false});
+            return false;
+        }
+
+        if(index==stones.size()-1)
+            return true;
+
+        int step_a=step-1,step_b=step,step_c=step+1;
+
+        string key_a,key_b,key_c;
+        key_a=key_b=key_c=to_string(stones[index])+'-';
+
+        if(step_a>0)
+        {
+            key_a+=to_string(step_a);
+            auto f=table.find(key_a);
+            if(f==table.end())
+            {
+                if(core(stones,index+1,step_a,table))
+                    return true;
+            }
+            else if(f->second)
+                return true;
+        }
+        if(step_b>0)
+        {
+            key_b+=to_string(step_b);
+            auto f=table.find(key_b);
+            if(f==table.end())
+            {
+                if(core(stones,index+1,step_b,table))
+                    return true;
+            }
+            else if(f->second)
+                return true;
+        }
+        if(step_c>0)
+        {
+            key_c+=to_string(step_c);
+            auto f=table.find(key_c);
+            if(f==table.end())
+            {
+                if(core(stones,index+1,step_c,table))
+                    return true;
+            }
+            else if(f->second)
+                return true;
+        }
+        table.insert({to_string(stones[old_index])+'-'+to_string(step),false});
+        return false;
     }
 };
 
 int main()
 {
-    string str("sd2[f2[e]g]i");
-    Solution so;
-    so.decodeString(str);
+    vector<int> arr{0,1,3,5,6,8,12,17};
+    Solution s;
+    s.canCross(arr);
     return 0;
 }
