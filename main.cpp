5e8ba1d22ffee2b0d5947bc4139788a1327e4a40
mzb <331972146@qq.com>
2017-08-21 22:20:09 +0800

    459. Repeated Substring Pattern

--- main.cpp
+++ main.cpp
@@ -1,40 +1,25 @@
-#include "iostream"
 #include "string"
+#include "iostream"
 #include "vector"
-#include "algorithm"
 
 using namespace std;
 
 class Solution
 {
 public:
-    string shortestPalindrome(string s)
+    bool repeatedSubstringPattern(string &s)
     {
-        string two=s;
-        reverse(s.begin(),s.end());
-        two+="#";
-        two+=s;
-        vector<int> table(two.length(),0);
-        for(int i=1;i<two.length();++i)
+        vector<int> table(s.length(),0);
+        for(int i=1;i<s.length();++i)
         {
             int index=i;
-            while(table[index-1]!=0&&two[i]!=two[table[index-1]])
-            {
+            while(table[index-1]!=0&&s[i]!=s[table[index-1]])
                 index=table[index-1];
-            }
-            if(two[i]==two[table[index-1]])
+            if(s[i]==s[table[index-1]])
                 table[i]=table[index-1]+1;
             else
                 table[i]=table[index-1];
         }
-        return s.substr(0,s.length()-table.back())+two.substr(0,s.length());
+        return table.back()&&s.length()%(s.length()-table.back())==0;
     }
 };
-
-int main()
-{
-    Solution s;
-    string str("aacecaaa");
-    cout<<s.shortestPalindrome(str)<<endl;
-    return 0;
-}
