e62983b5812c20b2dbe63458703d252aa88dc4ec
mzb <331972146@qq.com>
2017-08-22 09:50:07 +0800

    33. Search in Rotated Sorted Array

--- main.cpp
+++ main.cpp
@@ -1,25 +1,35 @@
-#include "string"
 #include "iostream"
 #include "vector"
 
 using namespace std;
-
 class Solution
 {
 public:
-    bool repeatedSubstringPattern(string &s)
+    int search(vector<int> &nums,int target)
     {
-        vector<int> table(s.length(),0);
-        for(int i=1;i<s.length();++i)
+        if(nums.empty())
+            return -1;
+        int beg=0,end=nums.size()-1;
+        while(beg<=end)
         {
-            int index=i;
-            while(table[index-1]!=0&&s[i]!=s[table[index-1]])
-                index=table[index-1];
-            if(s[i]==s[table[index-1]])
-                table[i]=table[index-1]+1;
-            else
-                table[i]=table[index-1];
+            int mid=(beg+end)/2;
+            if(target==nums[mid])
+                return mid;
+            if(nums[mid]>=nums[0])//left
+            {
+                if(target<nums[mid]&&target>=nums[beg])
+                    end=mid-1;
+                else
+                    beg=mid+1;
+            }
+            else if(nums[mid]<nums[0])//right
+            {
+                if(target>nums[mid]&&target<=nums[end])
+                    beg=mid+1;
+                else
+                    end=mid-1;
+            }
         }
-        return table.back()&&s.length()%(s.length()-table.back())==0;
+        return -1;
     }
 };
