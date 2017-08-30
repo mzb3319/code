43f8269fc67d11a9393837ee52f46d0e7f3aa368
mzb <331972146@qq.com>
2017-08-30 09:56:02 +0800

    84. Largest Rectangle in Histogram

--- main.cpp
+++ main.cpp
@@ -6,53 +6,44 @@ using namespace std;
 class Solution
 {
 public:
-    bool exist(vector<vector<char>> &board,string &word)
+    int largestRectangleArea(vector<int> &heights)
     {
-        vector<vector<bool>> status(board.size(),vector<bool>(board[0].size(),false));
-        for(int i=0;i<board.size();++i)
+        if(heights.empty())
+            return 0;
+        vector<int> left2right(heights.size(),0),right2left(heights.size(),0);
+        left2right[0]=-1;
+        right2left[heights.size()-1]=heights.size();
+        /*
+         * 找到左边和右边第一个小于当前值的位置,如果某一点a的值大于当前值,那么如果点a左边第一个小于a的值
+         * 都大于或等于当前值,则继续比较a点左边的值,a点右边的值肯定都大于或等于当前值
+         */
+        for(int i=1;i<heights.size();++i)
         {
-            for(int j=0;j<board[0].size();++j)
-            {
-                if(search(board,word,i,j,0,status))
-                {
-                    return true;
-                }
-            }
+            int p=i-1;
+            while(p>=0&&heights[p]>=heights[i])
+                p=left2right[p];
+            left2right[i]=p;
         }
-        return false;
-    }
-private:
-    bool search(vector<vector<char>> &board,string &word,int i,int j,int index,vector<vector<bool>> &status)
-    {
-        if(index==word.size())
-            return true;
-        if(i<0||i>=board.size()||j<0||j>=board[0].size())
-            return false;
-        if(status[i][j])
-            return false;
-        if(board[i][j]!=word[index])
-            return false;
-        status[i][j]=true;
-        if(
-                search(board,word,i+1,j,index+1,status)||
-                search(board,word,i-1,j,index+1,status)||
-                search(board,word,i,j+1,index+1,status)||
-                search(board,word,i,j-1,index+1,status)
-                )
+        for(int i=heights.size()-2;i>=0;--i)
         {
-            status[i][j]=false;
-            return true;
+            int p=i+1;
+            while(p<heights.size()&&heights[p]>=heights[i])
+                p=right2left[p];
+            right2left[i]=p;
         }
-        status[i][j]=false;
-        return false;
+        int maxArea=INT32_MIN;
+        for(int i=0;i<heights.size();++i)
+        {
+            maxArea=max(maxArea,heights[i]*(right2left[i]-left2right[i]-1));
+        }
+        return maxArea;
     }
 };
 
 int main()
 {
+    vector<int> heights{1,1};
     Solution s;
-    vector<vector<char>> board{{'C','A','A'},{'A','A','A'},{'B','C','D'}};
-    string word="AAB";
-    s.exist(board,word);
+    s.largestRectangleArea(heights);
     return 0;
 }
