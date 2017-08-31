5c60aad49293e9f009a39dc6a2fa7970ed74a5e7
mzb <331972146@qq.com>
2017-08-31 09:22:09 +0800

    104. Maximum Depth of Binary Tree

--- main.cpp
+++ main.cpp
@@ -14,43 +14,11 @@ struct TreeNode
 class Solution
 {
 public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+    int maxDepth(TreeNode *root)
     {
-        vector<vector<int>> ret;
         if(root==NULL)
-            return ret;
-        vector<deque<TreeNode*>> table(2);
-        bool direction=true;//true->left2right,false->right2left
-        table[1].push_back(root);
-        vector<int> currLevel;
-        while(!table[direction].empty())
-        {
-            TreeNode *tmp=table[direction].front();
-            table[direction].pop_front();
-            currLevel.push_back(tmp->val);
-            if(direction)
-            {
-                if(tmp->left!=NULL)
-                    table[!direction].push_front(tmp->left);
-                if(tmp->right!=NULL)
-                    table[!direction].push_front(tmp->right);
-            }
-            else
-            {
-                if(tmp->right!=NULL)
-                    table[!direction].push_front(tmp->right);
-                if(tmp->left!=NULL)
-                    table[!direction].push_front(tmp->left);
-            }
-            if(table[direction].empty())
-            {
-                ret.push_back(currLevel);
-                currLevel.clear();
-                table[direction].clear();
-                direction=!direction;
-            }
-        }
-        return ret;
+            return 0;
+        return max(maxDepth(root->left),maxDepth(root->right))+1;
     }
 };
 
