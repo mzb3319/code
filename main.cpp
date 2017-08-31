328a24114871694f80762b7ccb3cc3d52f69ace1
mzb <331972146@qq.com>
2017-08-31 10:43:04 +0800

    124. Binary Tree Maximum Path Sum

--- main.cpp
+++ main.cpp
@@ -1,41 +1,41 @@
 #include "vector"
 #include "iostream"
-#include "deque"
+#include "unordered_map"
 
 using namespace std;
 
-struct TreeLinkNode
+struct TreeNode
 {
     int val;
-    TreeLinkNode *left;
-    TreeLinkNode *right;
-    TreeLinkNode *next;
-    TreeLinkNode(int a):val(a),left(NULL),right(NULL),next(NULL){}
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode *next;
+    TreeNode(int a):val(a),left(NULL),right(NULL),next(NULL){}
 };
 
 class Solution
 {
 public:
-    vector<vector<int>> generate(int numRows)
+    int maxPathSum(TreeNode *root)
     {
-        if(numRows<1)
-            return {};
-        vector<vector<int>> ret{{1}};
-        for(int i=1;i<numRows;++i)
-        {
-            int a=0,b=0;
-            vector<int> curr;
-            for(int j=0;j<=ret[i-1].size();++j)
-            {
-                a=b;
-                if(j<ret[i-1].size())
-                    b=ret[i-1][j];
-                else
-                    b=0;
-                curr.push_back(a+b);
-            }
-            ret.push_back(curr);
-        }
+        int ret=INT32_MIN;
+        core(root,ret);
         return ret;
+    }
+private:
+    int core(TreeNode *node,int &ret)
+    {
+        if(node==NULL)
+            return 0;
+        int left=core(node->left,ret);
+        int right=core(node->right,ret);
+        int tmp=node->val;
+        if(left>0)
+            tmp+=left;
+        if(right>0)
+            tmp+=right;
+        ret=max(tmp,ret);
+        tmp=max(left,right);
+        return tmp>0?tmp+node->val:node->val;
     }
 };
