90ecbd1a0775001467d64d97a57888ec9e1b49ea
mzb <331972146@qq.com>
2017-09-04 08:09:49 +0800

    128. Longest Consecutive Sequence

--- main.cpp
+++ main.cpp
@@ -1,69 +1,57 @@
-#include "vector"
 #include "iostream"
+#include "vector"
+#include "algorithm"
+#include "set"
 #include "unordered_set"
-#include "bitset"
-#include "deque"
 
 using namespace std;
 
 class Solution
 {
 public:
-    int ladderLength(string &beginWord,string &endWord,vector<string> &wordList)
+    int longestConsecutive1(vector<int> &nums)
     {
-        unordered_set<string> table(wordList.begin(),wordList.end());
-        if(table.find(endWord)==table.end())
+        if(nums.empty())
             return 0;
-        deque<string> q{beginWord};
-        int ret=0,currCount=1,nextCount=0;
-        while(!q.empty())
+        set<int> table;
+        for(int n:nums)
+            table.insert(n);
+        vector<int> _nums(table.begin(),table.end());
+        int maxLen=1,currLen=1;
+        for(int i=1;i<_nums.size();++i)
         {
-            if(q.front()==endWord)
-                return ret+1;
-            int count=getNext(q.front(),table,q);
-            nextCount+=count;
-            --currCount;
-            q.pop_front();
-            if(currCount==0)
+            if(_nums[i]==_nums[i-1]+1)
             {
-                ++ret;
-                currCount=nextCount;
-                nextCount=0;
+                ++currLen;
+                maxLen=max(maxLen,currLen);
+            }
+            else
+            {
+                maxLen=max(maxLen,currLen);
+                currLen=1;
             }
         }
-        return 0;
+        return maxLen;
     }
-private:
-    int getNext(string a,unordered_set<string> &table,deque<string> &q)
+    int longestConsecutive(vector<int> &nums)
     {
-        int count=0;
-        for(int i=0;i<a.size();++i)
+        if(nums.empty())
+            return 0;
+        unordered_set<int> table(nums.begin(),nums.end());
+        int maxLen=0;
+        for(int i=0;i<nums.size();++i)
         {
-            for(int j=0;j<26;++j)
-            {
-                char c=a[i];
-                if(c-'a'!=j)
-                {
-                    a[i]=j+'a';
-                    if(table.find(a)!=table.end())
-                    {
-                        q.push_back(a);
-                        ++count;
-                        table.erase(a);
-                    }
-                    a[i]=c;
-                }
-            }
+            auto f=table.find(nums[i]);
+            if(f==table.end())
+                continue;
+            table.erase(nums[i]);
+            int pre=nums[i]-1,next=nums[i]+1;
+            while(table.find(pre)!=table.end())
+                table.erase(pre--);
+            while(table.find(next)!=table.end())
+                table.erase(next++);
+            maxLen=max(maxLen,next-pre-1);
         }
-        return count;
+        return maxLen;
     }
 };
-
-int main()
-{
-    string bw="leet",ew="code";
-    vector<string> wl{"lest","leet","lose","code","lode","robe","lost"};
-    Solution s;
-    cout<<s.ladderLength(bw,ew,wl);
-    return 0;
-}
