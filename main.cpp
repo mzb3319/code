c88e5bddfe035ba54a92e50cb35933b1fe07df0f
mzb <331972146@qq.com>
2017-09-05 09:44:41 +0800

    131. Palindrome Partitioning

--- main.cpp
+++ main.cpp
@@ -1,57 +1,49 @@
 #include "iostream"
 #include "vector"
 #include "algorithm"
-#include "set"
-#include "unordered_set"
+#include "unordered_map"
 
 using namespace std;
 
 class Solution
 {
 public:
-    int longestConsecutive1(vector<int> &nums)
+    vector<vector<string>> partition(string &s)
     {
-        if(nums.empty())
-            return 0;
-        set<int> table;
-        for(int n:nums)
-            table.insert(n);
-        vector<int> _nums(table.begin(),table.end());
-        int maxLen=1,currLen=1;
-        for(int i=1;i<_nums.size();++i)
-        {
-            if(_nums[i]==_nums[i-1]+1)
-            {
-                ++currLen;
-                maxLen=max(maxLen,currLen);
-            }
-            else
-            {
-                maxLen=max(maxLen,currLen);
-                currLen=1;
-            }
-        }
-        return maxLen;
+        vector<vector<string>> ret;
+        vector<string> tmp;
+        core(s,0,tmp,ret);
+        return ret;
     }
-    int longestConsecutive(vector<int> &nums)
+private:
+    void core(string &s,int index,vector<string> &tmp,vector<vector<string>> &ret)
     {
-        if(nums.empty())
-            return 0;
-        unordered_set<int> table(nums.begin(),nums.end());
-        int maxLen=0;
-        for(int i=0;i<nums.size();++i)
+        if(index==s.length())
         {
-            auto f=table.find(nums[i]);
-            if(f==table.end())
-                continue;
-            table.erase(nums[i]);
-            int pre=nums[i]-1,next=nums[i]+1;
-            while(table.find(pre)!=table.end())
-                table.erase(pre--);
-            while(table.find(next)!=table.end())
-                table.erase(next++);
-            maxLen=max(maxLen,next-pre-1);
+            ret.push_back(tmp);
+            return;
         }
-        return maxLen;
+        for(int i=index;i<s.length();++i)
+        {
+            int l=index,r=i;
+            while(l<=r&&s[l]==s[r])
+            {
+                l++;
+                --r;
+            }
+            if(l>r)
+            {
+                tmp.push_back(s.substr(index,i-index+1));
+                core(s,i+1,tmp,ret);
+                tmp.pop_back();
+            }
+        }
     }
 };
+int main()
+{
+    unsigned  char a=0x80;
+    unsigned char b=a>>5;
+    cout<<(int)b<<endl;
+    return 0;
+}
