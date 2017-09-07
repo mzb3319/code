88ec49720758b8c1c8687039648d1e69fa07078e
mzb <mzb3319@163.com>
2017-09-07 22:15:33 +0800

    198. House Robber

--- main.cpp
+++ main.cpp
@@ -6,13 +6,22 @@ using namespace std;
 class Solution
 {
 public:
-    int hammingWeight(uint32_t n)
+    int rob(vector<int> &nums)
     {
+        if(nums.empty())
+            return 0;
+        vector<int> moneys(nums.size(),0);
         int ret=0;
-        while(n)
+        for(int i=0;i<nums.size();++i)
         {
-            ++ret;
-            n=(n-1)&n;
+            int a=0;
+            if(i-2>=0)
+                a=moneys[i-2];
+            int b=0;
+            if(i-3>=0)
+                b=moneys[i-3];
+            moneys[i]=nums[i]+max(a,b);
+            ret=max(ret,moneys[i]);
         }
         return ret;
     }
