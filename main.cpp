9434ed47ce3c30e63d5a4d9d389b5305feff471a
mzb <mzb3319@163.com>
2017-09-10 10:14:59 +0800

    208. Implement Trie (Prefix Tree)

--- main.cpp
+++ main.cpp
@@ -1,69 +1,52 @@
 #include "iostream"
 #include "vector"
 #include "unordered_map"
-#include "unordered_set"
+#include "string"
 
 using namespace std;
 
-struct course
-{
-    int num;
-    int color;
-    int order;
-    course(int n):num(n),color(0),order(0){}
-};
-
-class Solution
+class Trie
 {
 public:
-    vector<int> findOrder(int numCourses,vector<pair<int,int>> &prerequisites)
+    Trie():child(26, nullptr),isLeaf(false)
     {
-        vector<int> courses(numCourses,0);
-        vector<bool> visited(numCourses,false);
-        unordered_map<int,vector<int>> table;
-        unordered_set<int> path;
-        for(auto &it:prerequisites)
-            table[it.first].push_back(it.second);
-        int n=0;
-        for(int i=0;i<numCourses;++i)
-        {
-            if(visited[i])
-                continue;
-            if(!core(courses,visited,table,i,n,path))
-                return {};
-        }
-        vector<int> ret(numCourses);
-        for(int i=0;i<numCourses;++i)
-        {
-            ret[courses[i]]=i;
-        }
-        return ret;
+
     }
-private:
-    bool core(vector<int> &courses,vector<bool> &visited,unordered_map<int,vector<int>> &table,int course,int &n,unordered_set<int> &path)
+
+    void insert(string word)
     {
-        if(path.find(course)!=path.end())
-            return false;
-        if(visited[course])
-            return true;
-        visited[course]=true;
-        path.insert(course);
-        for(auto &it:table[course])
+        insert(word,0);
+    }
+
+    bool search(string word)
+    {
+        return search(word,0,true);
+    }
+
+    bool startsWith(string prefix)
+    {
+        return search(prefix,0,false);
+    }
+
+private:
+    bool isLeaf;
+    vector<Trie*> child;
+
+    void insert(string &word,int index)
+    {
+        if(index==word.length())
         {
-            if(!core(courses,visited,table,it,n,path))
-                return false;
+            isLeaf=true;
+            return;
         }
-        path.erase(course);
-        courses[course]=n;
-        ++n;
-        return true;
+        if(child[word[index]-'a']== nullptr)
+            child[word[index]-'a']=new Trie;
+        child[word[index]-'a']->insert(word,index+1);
+    }
+    bool search(string &word,int index,bool needLeaf)
+    {
+        if(index==word.length())
+            return needLeaf?isLeaf:true;
+        return child[word[index]-'a']==nullptr?false:child[word[index]-'a']->search(word,index+1,needLeaf);
     }
 };
-
-int main()
-{
-    vector<pair<int,int>> prere{{1,0}};
-    Solution s;
-    s.findOrder(2,prere);
-    return 0;
-}
