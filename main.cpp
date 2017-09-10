929604d1655e1504fc720ab88f9d8615f84a39b2
mzb <mzb3319@163.com>
2017-09-10 14:51:15 +0800

    212. Word Search II

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "iostream"
 #include "vector"
-#include "unordered_map"
+#include "unordered_set"
 #include "string"
 
 using namespace std;
@@ -13,26 +13,26 @@ public:
 
     }
 
-    void insert(string word)
+    void insert(const string &word)
     {
         insert(word,0);
     }
 
-    bool search(string word)
+    Trie* search(char c)
     {
-        return search(word,0,true);
+        return child[c-'a'];
     }
 
-    bool startsWith(string prefix)
+    bool isWord()
     {
-        return search(prefix,0,false);
+        return isLeaf;
     }
 
 private:
     bool isLeaf;
     vector<Trie*> child;
 
-    void insert(string &word,int index)
+    void insert(const string &word,int index)
     {
         if(index==word.length())
         {
@@ -43,10 +43,51 @@ private:
             child[word[index]-'a']=new Trie;
         child[word[index]-'a']->insert(word,index+1);
     }
-    bool search(string &word,int index,bool needLeaf)
+};
+
+class Solution
+{
+public:
+    vector<string> findWords(vector<vector<char>> &board,vector<string> &words)
     {
-        if(index==word.length())
-            return needLeaf?isLeaf:true;
-        return child[word[index]-'a']==nullptr?false:child[word[index]-'a']->search(word,index+1,needLeaf);
+        Trie wordTree;
+        for(const string &word:words)
+            wordTree.insert(word);
+        unordered_set<string> ret;
+        vector<vector<bool>> visited(board.size(),vector<bool>(board.front().size(),false));
+        string path;
+        for(int i=0;i<board.size();++i)
+        {
+            for(int j=0;j<board.front().size();++j)
+            {
+                check(board,i,j,ret,&wordTree,path,visited);
+            }
+        }
+        return vector<string>(ret.begin(),ret.end());
+    }
+private:
+    void check(vector<vector<char>> &board,int x,int y,unordered_set<string> &ret,Trie *curr,string &path,vector<vector<bool>> &visited)
+    {
+        if(x<0||y<0||x>=board.size()||y>=board.front().size()||visited[x][y])
+            return;
+
+        Trie *tmp= nullptr;
+        tmp=curr->search(board[x][y]);
+
+        if(tmp== nullptr)
+            return;
+
+        path.push_back(board[x][y]);
+
+        visited[x][y]=true;
+        check(board,x+1,y,ret,tmp,path,visited);
+        check(board,x-1,y,ret,tmp,path,visited);
+        check(board,x,y+1,ret,tmp,path,visited);
+        check(board,x,y-1,ret,tmp,path,visited);
+        visited[x][y]=false;
+
+        if(tmp->isWord())
+            ret.insert(path);
+        path.pop_back();
     }
 };
