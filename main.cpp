e70c21b37b05c2e649fc56d2aebd03e4e2552720
mzb <mzb3319@163.com>
2017-09-10 16:47:30 +0800

    217. Contains Duplicate

--- main.cpp
+++ main.cpp
@@ -1,93 +1,21 @@
 #include "iostream"
 #include "vector"
 #include "unordered_set"
-#include "string"
 
 using namespace std;
-
-class Trie
-{
-public:
-    Trie():child(26, nullptr),isLeaf(false)
-    {
-
-    }
-
-    void insert(const string &word)
-    {
-        insert(word,0);
-    }
-
-    Trie* search(char c)
-    {
-        return child[c-'a'];
-    }
-
-    bool isWord()
-    {
-        return isLeaf;
-    }
-
-private:
-    bool isLeaf;
-    vector<Trie*> child;
-
-    void insert(const string &word,int index)
-    {
-        if(index==word.length())
-        {
-            isLeaf=true;
-            return;
-        }
-        if(child[word[index]-'a']== nullptr)
-            child[word[index]-'a']=new Trie;
-        child[word[index]-'a']->insert(word,index+1);
-    }
-};
 
 class Solution
 {
 public:
-    vector<string> findWords(vector<vector<char>> &board,vector<string> &words)
+    bool containsDuplicate(vector<int> &nums)
     {
-        Trie wordTree;
-        for(const string &word:words)
-            wordTree.insert(word);
-        unordered_set<string> ret;
-        vector<vector<bool>> visited(board.size(),vector<bool>(board.front().size(),false));
-        string path;
-        for(int i=0;i<board.size();++i)
+        unordered_set<int> table;
+        for(int n:nums)
         {
-            for(int j=0;j<board.front().size();++j)
-            {
-                check(board,i,j,ret,&wordTree,path,visited);
-            }
+            auto is=table.insert(n);
+            if(!is.second)
+                return true;
         }
-        return vector<string>(ret.begin(),ret.end());
-    }
-private:
-    void check(vector<vector<char>> &board,int x,int y,unordered_set<string> &ret,Trie *curr,string &path,vector<vector<bool>> &visited)
-    {
-        if(x<0||y<0||x>=board.size()||y>=board.front().size()||visited[x][y])
-            return;
-
-        Trie *tmp= nullptr;
-        tmp=curr->search(board[x][y]);
-
-        if(tmp== nullptr)
-            return;
-
-        path.push_back(board[x][y]);
-
-        visited[x][y]=true;
-        check(board,x+1,y,ret,tmp,path,visited);
-        check(board,x-1,y,ret,tmp,path,visited);
-        check(board,x,y+1,ret,tmp,path,visited);
-        check(board,x,y-1,ret,tmp,path,visited);
-        visited[x][y]=false;
-
-        if(tmp->isWord())
-            ret.insert(path);
-        path.pop_back();
+        return false;
     }
 };
