bb2256bebd74336f9207d68c7d74db1eea32bf0f
mzb <mzb3319@163.com>
2017-09-12 18:29:59 +0800

    295. Find Median from Data Stream

--- main.cpp
+++ main.cpp
@@ -1,21 +1,57 @@
 #include "iostream"
 #include "vector"
-#include "unordered_set"
+#include "queue"
 
 using namespace std;
 
-class Solution
+class MedianFinder
 {
 public:
-    bool containsDuplicate(vector<int> &nums)
+    MedianFinder()
     {
-        unordered_set<int> table;
-        for(int n:nums)
-        {
-            auto is=table.insert(n);
-            if(!is.second)
-                return true;
-        }
-        return false;
+
     }
+
+    void addNum(int num)
+    {
+        if(!left.empty()&&num<=left.top())
+            left.push(num);
+        else if(!right.empty()&&num>=right.top())
+            right.push(num);
+        else
+            left.push(num);
+        while(left.size()<right.size())
+        {
+            int n=right.top();
+            left.push(n);
+            right.pop();
+        }
+        while(left.size()>right.size()+1)
+        {
+            int n=left.top();
+            right.push(n);
+            left.pop();
+        }
+    }
+
+    double findMedian()
+    {
+        if(left.size()>right.size())
+            return left.top();
+        double a=left.top(),b=right.top();
+        return (a+b)/2;
+    }
+
+private:
+    priority_queue<int> left;
+    priority_queue<int,vector<int>,greater<int>> right;
 };
+
+int main()
+{
+    MedianFinder m;
+    m.addNum(1);
+    m.addNum(2);
+    cout<<m.findMedian()<<endl;
+    return 0;
+}
