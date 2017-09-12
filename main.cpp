5c442a4c0a6b304354c8bd306bd96e57a0bb7a6f
mzb <331972146@qq.com>
2017-09-12 12:06:02 +0800

    240. Search a 2D Matrix II

--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
-#include "iostream"
+#include "istream"
 #include "vector"
-#include "deque"
 
 using namespace std;
 
 class Solution
 {
 public:
-    vector<int> maxSlidingWindow(vector<int> &nums,int k)
+    bool searchMatrix(vector<vector<int>> &matrix,int target)
     {
-        vector<int> ret;
-        deque<int> window;
-        for(int i=0;i<nums.size();++i)
+        if(matrix.empty()||matrix.front().empty())
+            return false;
+        int row=0,col=matrix.front().size()-1;
+        while(row<matrix.size()&&col>=0)
         {
-            while(!window.empty()&&(nums[i]>nums[window.front()]||i-window.front()>=k))
-                window.pop_front();
-            while(!window.empty()&&(nums[i]>nums[window.back()]))
-                window.pop_back();
-            window.push_back(i);
-            if(i>=k-1)
-                ret.push_back(nums[window.front()]);
+            if(matrix[row][col]==target)
+                return true;
+            else if(matrix[row][col]<target)
+                ++row;
+            else
+                --col;
         }
-        return ret;
+
+        return false;
     }
 };
