9f929eeaaed46b07f30fa41e3b7d37a465a35722
mzb <331972146@qq.com>
2017-09-12 14:02:28 +0800

    289. Game of Life

--- main.cpp
+++ main.cpp
@@ -6,20 +6,54 @@ using namespace std;
 class Solution
 {
 public:
-    void moveZeroes(vector<int> &nums)
+    void gameOfLife(vector<vector<int>> &board)
     {
-        int fast=0,slow=0;
-        while(fast<nums.size())
+        for(int i=0;i<board.size();++i)
         {
-            while(slow<fast&&nums[slow]!=0)
-                ++slow;
-            if(nums[fast]!=0)
+            for(int j=0;j<board.front().size();++j)
             {
-                int tmp=nums[slow];
-                nums[slow]=nums[fast];
-                nums[fast]=tmp;
+                //0->dead;1->live;-1->dead2live;2->live2dead
+                int count=0;
+                //top
+                if(i-1>=0&&board[i-1][j]>0)
+                    ++count;
+                //bottom
+                if(i+1<board.size()&&board[i+1][j]>0)
+                    ++count;
+                //left
+                if(j-1>=0&&board[i][j-1]>0)
+                    ++count;
+                //right
+                if(j+1<board.front().size()&&board[i][j+1]>0)
+                    ++count;
+                //top-left
+                if(i-1>=0&&j-1>=0&&board[i-1][j-1]>0)
+                    ++count;
+                //top-right
+                if(i-1>=0&&j+1<board.front().size()&&board[i-1][j+1]>0)
+                    ++count;
+                //bottom-left
+                if(i+1<board.size()&&j-1>=0&&board[i+1][j-1]>0)
+                    ++count;
+                //bottom-right
+                if(i+1<board.size()&&j+1<board.front().size()&&board[i+1][j+1]>0)
+                    ++count;
+
+                if(board[i][j]==0&&count==3)
+                    board[i][j]=-1;
+                else if(board[i][j]==1&&(count<2||count>3))
+                    board[i][j]=2;
             }
-            ++fast;
+        }
+        for(int i=0;i<board.size();++i)
+        {
+            for(int j=0;j<board.front().size();++j)
+            {
+                if(board[i][j]==-1)
+                    board[i][j]=1;
+                else if(board[i][j]==2)
+                    board[i][j]=0;
+            }
         }
     }
 };
