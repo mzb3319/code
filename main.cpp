353ca82f4c4f6b536f0e40221e85cb3fb0674588
mzb <331972146@qq.com>
2017-09-12 11:51:50 +0800

    239. Sliding Window Maximum

--- main.cpp
+++ main.cpp
@@ -1,37 +1,25 @@
 #include "iostream"
 #include "vector"
+#include "deque"
 
 using namespace std;
 
 class Solution
 {
 public:
-    vector<int> productExceptSelf(vector<int> &nums)
+    vector<int> maxSlidingWindow(vector<int> &nums,int k)
     {
-        vector<int> left2right,right2left;
-        int num=1;
-        for(int n:nums)
-        {
-            num*=n;
-            left2right.push_back(num);
-        }
-        num=1;
-        for(int i=nums.size()-1;i>=0;--i)
-        {
-            num*=nums[i];
-            right2left.push_back(num);
-        }
         vector<int> ret;
+        deque<int> window;
         for(int i=0;i<nums.size();++i)
         {
-            int a=1;
-            if(i-1>=0)
-                a=left2right[i-1];
-            int b=1;
-            int index=nums.size()-i-1;
-            if(index-1>=0)
-                b=right2left[index-1];
-            ret.push_back(a*b);
+            while(!window.empty()&&(nums[i]>nums[window.front()]||i-window.front()>=k))
+                window.pop_front();
+            while(!window.empty()&&(nums[i]>nums[window.back()]))
+                window.pop_back();
+            window.push_back(i);
+            if(i>=k-1)
+                ret.push_back(nums[window.front()]);
         }
         return ret;
     }
