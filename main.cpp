70b04df850db10f3f85e95ce40f9e1c31daa17e9
mzb <331972146@qq.com>
2017-09-12 09:59:36 +0800

    238. Product of Array Except Self

--- main.cpp
+++ main.cpp
@@ -3,22 +3,36 @@
 
 using namespace std;
 
-struct ListNode
-{
-    int val;
-    ListNode *next;
-
-};
-
 class Solution
 {
 public:
-    void deleteNode(ListNode *node)
+    vector<int> productExceptSelf(vector<int> &nums)
     {
-        if(node== nullptr)
-            return;
-        node->val=node->next->val;
-        node->next=node->next->next;
-        delete node->next;
+        vector<int> left2right,right2left;
+        int num=1;
+        for(int n:nums)
+        {
+            num*=n;
+            left2right.push_back(num);
+        }
+        num=1;
+        for(int i=nums.size()-1;i>=0;--i)
+        {
+            num*=nums[i];
+            right2left.push_back(num);
+        }
+        vector<int> ret;
+        for(int i=0;i<nums.size();++i)
+        {
+            int a=1;
+            if(i-1>=0)
+                a=left2right[i-1];
+            int b=1;
+            int index=nums.size()-i-1;
+            if(index-1>=0)
+                b=right2left[index-1];
+            ret.push_back(a*b);
+        }
+        return ret;
     }
 };
