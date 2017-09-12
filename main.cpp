61e71f8461acdd70c0d7529f7fdf25d13141b76d
mzb <mzb3319@163.com>
2017-09-12 22:51:46 +0800

    322. Coin Change

--- main.cpp
+++ main.cpp
@@ -1,26 +1,42 @@
 #include "iostream"
 #include "vector"
+#include "unordered_map"
 
 using namespace std;
 
 class Solution
 {
 public:
-    int lengthOfLIS(vector<int> &nums)
+    int coinChange(vector<int> &coins,int amount)
     {
-        int ret=0;
-        vector<int> table(nums.size(),0);
-        for(int i=nums.size()-1;i>=0;--i)
-        {
-            int n=0;
-            for(int j=i+1;j<nums.size();++j)
-            {
-                if(nums[i]<nums[j])
-                    n=max(table[j],n);
-            }
-            table[i]=n+1;
-            ret=max(table[i],ret);
-        }
-        return ret;
+        //unordered_map<int,int> table;
+        vector<int> table(amount+1,-2);
+        int ret=core(coins,amount,table);
+        return ret==INT32_MAX?-1:ret;
+    }
+private:
+    int core(vector<int> &coins,int amount,vector<int> &table)
+    {
+        if(amount<0)
+            return INT32_MAX;
+        if(amount==0)
+            return 0;
+        if(table[amount]!=-2)
+            return table[amount];
+        int need=INT32_MAX;
+        for(int i=0;i<coins.size();++i)
+            need=min(need,core(coins,amount-coins[i],table));
+        if(need!=INT32_MAX)
+            need+=1;
+        table[amount]=need;
+        return need;
     }
 };
+
+int main()
+{
+    Solution s;
+    vector<int> coins{1,2,5};
+    s.coinChange(coins,11);
+    return 0;
+}
