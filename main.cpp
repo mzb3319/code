08f8a204a4985a548fb98f59e4abdb5681dbe32a
mzb <mzb3319@163.com>
2017-09-13 22:00:38 +0800

    344. Reverse String

--- main.cpp
+++ main.cpp
@@ -1,78 +1,20 @@
 #include "iostream"
-#include "vector"
-#include "deque"
+#include "string"
 
 using namespace std;
 
-  // This is the interface that allows for creating nested lists.
-  // You should not implement it, or speculate about its implementation
-  class NestedInteger {
-    public:
-     // Return true if this NestedInteger holds a single integer, rather than a nested list.
-      bool isInteger() const;
-
-     // Return the single integer that this NestedInteger holds, if it holds a single integer
-     // The result is undefined if this NestedInteger holds a nested list
-     int getInteger() const;
-
-      // Return the nested list that this NestedInteger holds, if it holds a nested list
-      // The result is undefined if this NestedInteger holds a single integer
-      const vector<NestedInteger> &getList() const;
-  };
-class NestedIterator {
+class Solution
+{
 public:
-    NestedIterator(vector<NestedInteger> &nestedList):index(0){
-        core(nestedList);
-    }
-
-    int next() {
-        return table[index++];
-    }
-
-    bool hasNext() {
-        return index<table.size();
-    }
-private:
-    vector<int> table;
-    int index;
-    void core(const vector<NestedInteger> &list)
+    string reverseString(string s)
     {
-        for(int i=0;i<list.size();++i)
+        int beg=0,end=s.length()-1;
+        while(beg<end)
         {
-            if(list[i].isInteger())
-                table.push_back(list[i].getInteger());
-            else
-            {
-                core(list[i].getList());
-            }
+            char tmp=s[beg];
+            s[beg]=s[end];
+            s[end]=tmp;
         }
+        return s;
     }
-};
-
-class NestedIterator1 {
-public:
-    NestedIterator1(vector<NestedInteger> &nestedList):list(nestedList.begin(),nestedList.end()){
-
-    }
-
-    int next() {
-        int tmp=list.front().getInteger();
-        list.pop_front();
-        return tmp;
-    }
-
-    bool hasNext() {
-        while(!list.empty()&&!(list.front().isInteger()))
-        {
-            auto tmp=list.front().getList();
-            list.pop_front();
-            for(int i=tmp.size()-1;i>=0;--i)
-            {
-                list.push_front(tmp[i]);
-            }
-        }
-        return list.empty()?false:true;;
-    }
-private:
-    deque<NestedInteger> list;
 };
