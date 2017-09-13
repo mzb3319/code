1ebc424dd81f425c2f030d68695c3560b551cc04
mzb <331972146@qq.com>
2017-09-13 10:51:50 +0800

    328. Odd Even Linked List

--- main.cpp
+++ main.cpp
@@ -1,42 +1,35 @@
 #include "iostream"
 #include "vector"
-#include "unordered_map"
 
 using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+};
 
 class Solution
 {
 public:
-    int coinChange(vector<int> &coins,int amount)
+    ListNode *oddEvenList(ListNode *head)
     {
-        //unordered_map<int,int> table;
-        vector<int> table(amount+1,-2);
-        int ret=core(coins,amount,table);
-        return ret==INT32_MAX?-1:ret;
-    }
-private:
-    int core(vector<int> &coins,int amount,vector<int> &table)
-    {
-        if(amount<0)
-            return INT32_MAX;
-        if(amount==0)
-            return 0;
-        if(table[amount]!=-2)
-            return table[amount];
-        int need=INT32_MAX;
-        for(int i=0;i<coins.size();++i)
-            need=min(need,core(coins,amount-coins[i],table));
-        if(need!=INT32_MAX)
-            need+=1;
-        table[amount]=need;
-        return need;
+        if(head== nullptr)
+            return nullptr;
+        ListNode *p=head;
+        ListNode tmp,*even=&tmp;
+        while(p!= nullptr&&p->next!= nullptr)
+        {
+            even->next=p->next;
+            p->next=p->next->next;
+            p=p->next;
+            even=even->next;
+            even->next= nullptr;
+        }
+        p=head;
+        while(p->next!= nullptr)
+            p=p->next;
+        p->next=tmp.next;
+        return head;
     }
 };
-
-int main()
-{
-    Solution s;
-    vector<int> coins{1,2,5};
-    s.coinChange(coins,11);
-    return 0;
-}
