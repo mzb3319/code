cffabf1265d30c6011976168f087803f96eabb02
mzb <331972146@qq.com>
2017-09-14 08:34:30 +0800

    384. Shuffle an Array

--- main.cpp
+++ main.cpp
@@ -8,45 +8,35 @@
 
 using namespace std;
 
-class RandomizedSet {
+class Solution
+{
 public:
-    /** Initialize your data structure here. */
-    RandomizedSet() {
+    Solution (vector<int> nums):original(nums),shuffled(nums)
+    {
 
     }
 
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-    bool insert(int val) {
-        if(table.find(val)!=table.end())
-            return false;
-        table_v.push_back(val);
-        table[val]=table_v.size()-1;
-        return true;
+    vector<int> reset()
+    {
+        return original;
     }
 
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
-    bool remove(int val) {
-        if(table.find(val)==table.end())
-            return false;
-        table_v[table[val]]=table_v.back();
-        table[table_v.back()]=table[val];
-        table_v.pop_back();
-        table.erase(val);
-        return true;
-    }
-
-    /** Get a random element from the set. */
-    int getRandom() {
-        int r=random()%(table_v.size());
-        return table_v[r];
+    vector<int> shuffle()
+    {
+        for(int i=0;i<shuffled.size();++i)
+        {
+            int r=random()%shuffled.size();
+            swap(i,r);
+        }
+        return shuffled;
     }
 private:
-    unordered_map<int,int> table;
-    vector<int> table_v;
+    vector<int> original;
+    vector<int> shuffled;
+    void swap(int i,int r)
+    {
+        int tmp=shuffled[i];
+        shuffled[i]=shuffled[r];
+        shuffled[r]=tmp;
+    }
 };
-
-int main()
-{
-    cout<<random()%5<<endl;
-    return 0;
-}
