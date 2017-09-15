172c1fb7ddadb6ea08b0cfce7668df1cc08e15ef
mzb <mzb3319@163.com>
2017-09-15 18:48:59 +0800

    221. Maximal Square

--- main.cpp
+++ main.cpp
@@ -1,52 +1,49 @@
 #include "iostream"
-#include "string"
-#include "unordered_map"
 #include "vector"
-#include "algorithm"
-#include "queue"
-#include "unordered_set"
 
 using namespace std;
 
-class RandomizedSet {
+class Solution
+{
 public:
-    /** Initialize your data structure here. */
-    RandomizedSet() {
-
+    int maximalSquare(vector<string> &matrix)
+    {
+        if(matrix.empty()||matrix.front().empty())
+            return 0;
+        vector<vector<pair<int,int>>> table(matrix.size()+1,vector<pair<int,int>>(matrix.front().size()+1));
+        for(int i=matrix.size()-1;i>=0;--i)
+        {
+            for(int j=matrix.front().size()-1;j>=0;--j)
+            {
+                if(matrix[i][j]=='1')
+                {
+                    table[i][j].first+=table[i][j+1].first+1;
+                    table[i][j].second+=table[i+1][j].second+1;
+                }
+            }
+        }
+        int ret=0;
+        for(int i=0;i<matrix.size();++i)
+        {
+            for(int j=0;j<matrix.front().size();++j)
+            {
+                if(matrix[i][j]=='1')
+                {
+                    int len=1;
+                    while(i+len<matrix.size()&&j+len<matrix.front().size()&&table[i+len][j].first>len&&table[i][j+len].second>len)
+                        ++len;
+                    ret=max(ret,len*len);
+                }
+            }
+        }
+        return ret;
     }
-
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-    bool insert(int val) {
-        if(table.find(val)!=table.end())
-            return false;
-        table_v.push_back(val);
-        table[val]=table_v.size()-1;
-        return true;
-    }
-
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
-    bool remove(int val) {
-        if(table.find(val)==table.end())
-            return false;
-        table_v[table[val]]=table_v.back();
-        table[table_v.back()]=table[val];
-        table_v.pop_back();
-        table.erase(val);
-        return true;
-    }
-
-    /** Get a random element from the set. */
-    int getRandom() {
-        int r=random()%(table_v.size());
-        return table_v[r];
-    }
-private:
-    unordered_map<int,int> table;
-    vector<int> table_v;
 };
 
 int main()
 {
-    cout<<random()%5<<endl;
+    vector<string> matrix{"10100","10111","11111","10010"};
+    Solution s;
+    s.maximalSquare(matrix);
     return 0;
 }
